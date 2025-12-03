04c9a4277e3ab6f5b1d79b33a9dc8ce09ab4cf4d
ymxx-8 <yangmu363@gmail.com>
2025-12-03 18:21:10 +0800

    Update index.js

--- index.js
+++ index.js
@@ -18,8 +18,7 @@ const NEZHA_SERVER = process.env.NEZHA_SERVER || '';        // 哪吒v1填写形
 const NEZHA_PORT = process.env.NEZHA_PORT || '';            // 使用哪吒v1请留空，哪吒v0需填写
 const NEZHA_KEY = process.env.NEZHA_KEY || '';              // 哪吒v1的NZ_CLIENT_SECRET或哪吒v0的agent密钥
 const ARGO_DOMAIN = process.env.ARGO_DOMAIN || 'yangmu363.yangmu363.dpdns.org';          // 固定隧道域名,留空即启用临时隧道
-const ARGO_AUTH = process.env.ARGO_AUTH || 'eyJhIjoiYTczMmI4YmZhY2FiMWM2Y2IyZmM4ZTkwZDEzYzRkY2IiLCJ0IjoiNTMxNWEyNTYtZmNjMC00ZTBmLTk2YWQtZGE3MzIzMTdhMjI1IiwicyI6Ik56TTNabUl4TkRjdE56VTVOeTAwWWpaaUxXSTROekl0TW1NMk9XRXdOVFppTVRSaCJ9
-';              // 固定隧道密钥json或token,留空即启用临时隧道,json获取地址：https://json.zone.id
+const ARGO_AUTH = process.env.ARGO_AUTH || 'eyJhIjoiYTczMmI4YmZhY2FiMWM2Y2IyZmM4ZTkwZDEzYzRkY2IiLCJ0IjoiNTMxNWEyNTYtZmNjMC00ZTBmLTk2YWQtZGE3MzIzMTdhMjI1IiwicyI6Ik56TTNabUl4TkRjdE56VTVOeTAwWWpaaUxXSTROekl0TW1NMk9XRXdOVFppTVRSaCJ9';          // 固定隧道密钥json或token,留空即启用临时隧道,json获取地址：https://json.zone.id
 const ARGO_PORT = process.env.ARGO_PORT || 8001;            // 固定隧道端口,使用token需在cloudflare后台设置和这里一致
 const CFIP = process.env.CFIP || 'cdns.doon.eu.org';        // 节点优选域名或优选ip  
 const CFPORT = process.env.CFPORT || 443;                   // 节点优选域名或优选ip对应的端口
